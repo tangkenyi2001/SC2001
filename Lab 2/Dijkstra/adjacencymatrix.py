e13e0bea644325494c247ce608d6b2d5d489267c
tangkenyi2001 <73588464+tangkenyi2001@users.noreply.github.com>
2023-10-01 14:09:40 +0800

    commit

--- Lab 2/Dijkstra/adjacencymatrix.py
+++ Lab 2/Dijkstra/adjacencymatrix.py
@@ -35,7 +35,36 @@ class Graph(object):
         while i< self.size:
             print(self.adjMatrix[i])
             i+=1
-    
+
+'''class PriorityQueue(object):
+    def __init__(self):
+        self.queue = []
+ 
+    def __str__(self):
+        return ' '.join([str(i) for i in self.queue])
+ 
+    # for checking if the queue is empty
+    def isEmpty(self):
+        return len(self.queue) == 0
+ 
+    # for inserting an element in the queue
+    def insert(self, data):
+        self.queue.append(data)
+ 
+    # for popping an element based on Priority
+    def delete(self):
+        try:
+            max_val = 0
+            for i in range(len(self.queue)):
+                if self.queue[i] > self.queue[max_val]:
+                    max_val = i
+            item = self.queue[max_val]
+            del self.queue[max_val]
+            return item
+        except IndexError:
+            print()
+            exit()'''
+   
 
 
 def main():
@@ -45,8 +74,9 @@ def main():
     G.add_edge(1, 2, 4)
     G.add_edge(2, 0, 5)
     G.add_edge(2, 3, 9)
-
     G.print_matrix()
+    
+    
 
 
 if __name__ == '__main__':
