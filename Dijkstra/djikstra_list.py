8c29499758b8b7b43e8964542fd1c39a13d6fcd3
Justin <103985506+speaklouderplease@users.noreply.github.com>
2023-10-01 15:14:58 +0800

    swapped files

--- Dijkstra/djikstra_list.py
+++ Dijkstra/djikstra_list.py
@@ -1,47 +1,30 @@
 import numpy as np
+import heapq
 
-def getMin(s):
-    length = len(s)
-    minimum = s[0]
-    if length == 1:
-        return minimum
-    else:
-        for x in range(length):
-            if s[x] < minimum:
-                minimum = s[x]
-        return minimum
+class Graph_Adjacency:  # Adjacency list
+    def __init__(self, V):
+        self.V = V
+        self.adjList = [[] for _ in range(V)]
 
-
-def isQEmpty(Q):
-    if Q.len() == 0:
-        return 1
-    else:
-        return 0
+    def addEdge(self, u, v, weight):
+        self.adjList[u].append((v, weight))
+        self.adjList[v].append((u, weight))
     
 # Dijkstra with Adjacency List and Minimizing Heap as priority queue
-def dijkstra_AM(G, source, V):  # V is number of vertices.
-    Q = []  # priority queue
+def djikstra_AL(G, source):
+    Q = [] # priority queue
+    V = G.V  # assuming G.V gives number of vertices
     d, pi = []
     for x in range(V):
         d[x] = np.inf
         pi[x] = None
 
-    S = np.zeros(V)  # list of size V with all 0
     d[source] = 0
-
-    for v in range(V):  # put vertices in priority queue in increasing d[v] order
-        Q.append((v, d[v]))  # [0] is vertex. [1] is weight of vertex
-
-    while not isQEmpty(Q):
-        u = getMin(Q)
-        current_node = u[0]
-        S[current_node] = 1
-        Q.remove(u)
-
-        for v in range(V):
-            if v is not u and G[current_node][v] is not None:  # not itself and neighbour
-                if S[v] is not 1 and d[v] > d[current_node] + G[current_node][v]:
-                    Q.remove((v, d[v]))
-                    d[v] = d[current_node] + G[current_node][v]
-                    pi[v] = current_node
-                    Q.append((v, d[v]))
+    heapq.heappush(Q, (0, source))
+    while Q:
+        md, u = heapq.heappop(Q) # md is minimum distance of the smallest vertex
+        for v, weight in G.adjList[u]: # need to create G as Graph_Adjacency(G) for this to work
+            if d[v] > d[u] + weight:
+                d[v] = d[u] + weight
+                pi[v] = u
+                heapq.heappush(Q, (d[v], v))
