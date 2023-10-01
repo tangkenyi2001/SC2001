1fb296aba4d6e17227587110b602356640b0dfa8
tangkenyi2001 <73588464+tangkenyi2001@users.noreply.github.com>
2023-10-01 13:58:19 +0800

    update

--- Lab 2/Dijkstra/adjacencymatrix.py
+++ Lab 2/Dijkstra/adjacencymatrix.py
@@ -2,13 +2,14 @@
 
 
 class Graph(object):
-
+    
     # Initialize the matrix
     def __init__(self, size):
         self.adjMatrix = []
         for i in range(size):
             self.adjMatrix.append([0 for i in range(size)])
         self.size = size
+        self.V=self.size
 
     # Add edges with weight
     def add_edge(self, v1, v2,weight):
@@ -34,47 +35,20 @@ class Graph(object):
         while i< self.size:
             print(self.adjMatrix[i])
             i+=1
-        
-class PriorityQueue(object):
-    def __init__(self):
-        self.queue = []
- 
-    def __str__(self):
-        return ' '.join([str(i) for i in self.queue])
- 
-    # for checking if the queue is empty
-    def isEmpty(self):
-        return len(self.queue) == 0
- 
-    # for inserting an element in the queue
-    def insert(self, data):
-        self.queue.append(data)
- 
-    # for popping an element based on Priority
-    def delete(self):
-        try:
-            max_val = 0
-            for i in range(len(self.queue)):
-                if self.queue[i] > self.queue[max_val]:
-                    max_val = i
-            item = self.queue[max_val]
-            del self.queue[max_val]
-            return item
-        except IndexError:
-            print()
-            exit()
- 
+    
+
 
 def main():
-    g = Graph(5)
-    g.add_edge(0, 1,3)
-    g.add_edge(0, 2,3)
-    g.add_edge(1, 2,4)
-    g.add_edge(2, 0,5)
-    g.add_edge(2, 3,9)
+    G = Graph(5)
+    G.add_edge(0, 1, 3)
+    G.add_edge(0, 2, 3)
+    G.add_edge(1, 2, 4)
+    G.add_edge(2, 0, 5)
+    G.add_edge(2, 3, 9)
 
-    g.print_matrix()
+    G.print_matrix()
 
 
 if __name__ == '__main__':
     main()
+    
