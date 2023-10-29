de89ac9d162cfe5cfe9c010ee2bd51382aad3820
tangkenyi2001 <73588464+tangkenyi2001@users.noreply.github.com>
2023-10-29 17:11:31 +0800

    update

--- Lab 3/Knapsackdp.py
+++ Lab 3/Knapsackdp.py
@@ -4,26 +4,34 @@
 # be put in a knapsack of capacity W 
 
 
-def knapSack(C, wt, val, n): 
-    K=[[]*C for _ in range(n+1)]
-    for i in range(C):
-        K[0][i]=0
-    for i in range(n+1):
-        K[i][0]=0
-	# Build table K[][] in bottom up manner 
-	for i in range(n + 1): 
-		for w in range(C): 
-			if i == 0 or w == 0: 
-				K[i][w] = 0
-			elif wt[i-1] <= w: 
-				K[i][w] = max(val[i-1] 
-							+ K[i-1][w-wt[i-1]], 
-							K[i-1][w]) 
-			else: 
-				K[i][w] = K[i-1][w] 
+def knapSack(W, wt, val, n): 
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
+  
+    # Build table K[][] in bottom up manner 
+    for i in range(n + 1): 
+        for w in range(W + 1): 
+            if i == 0 or w == 0: 
+                K[i][w] = 0
+            elif wt[i-1] <= w: 
+                K[i][w] = max(val[i-1] 
+                              + K[i-1][w-wt[i-1]], 
+                              K[i-1][w]) 
+            else: 
+                K[i][w] = K[i-1][w] 
+    q = n
+    a = W
+    selected_items = []
 
-	return K[n][W] 
+    while q > 0 and a > 0:
+        if K[q][a] != K[q-1][a]:
+            selected_items.append(val[q-1])
+            a -= wt[q-1]
+        q -= 1
 
+    # Reverse the selected items list to get the correct order
+    selected_items.reverse()
+
+    return K[n][W], selected_items
 
 # Driver code 
 if __name__ == '__main__': 
@@ -31,5 +39,6 @@ if __name__ == '__main__':
 	weight = [5, 6, 8] 
 	C = 14
 	n = len(profit) 
-	print(knapSack(C, weight, profit, n)) 
-
+	max_value, selected_items = knapSack(C, weight, profit, n)
+print("Maximum value:", max_value)
+print("Selected items: Items that are worth ", selected_items)
