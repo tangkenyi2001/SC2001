46092909007428d7954e87796995e7472fac42e3
Justin <103985506+speaklouderplease@users.noreply.github.com>
2023-11-05 15:34:04 +0800

    update1.3

--- Lab 3/Knapsack_Recursive.py
+++ Lab 3/Knapsack_Recursive.py
@@ -9,16 +9,25 @@ def knapsack_recur(C, n, w, p):
     else:
         return max(p[n-1] + knapsack_recur(C-w[n-1], n-1, w, p) , knapsack_recur(C, n-1, w, p))
     
+def knapsack_recur_unbounded(C, n, w, p):
+    if n == 0 or C == 0:
+        return (C//w[0])*p[0]
+    not_taken = 0 + knapsack_recur(C, n-1, w, p)
+    taken = -100
+    if w[n] <= C:
+        taken = p[n] + knapsack_recur(C - w[n], n, w, p)
+    return max(taken, not_taken)
+    
 if __name__ == "__main__":
     C = 14 # capcacity of knapsack
     w = [5, 6, 8]
     p = [7, 6, 9]
     n = len(p) # number of objects
     start = timeit.default_timer()
-    print(knapsack_recur(C, n, w, p))
+    print(knapsack_recur_unbounded(C, n-1, w, p))
     end = timeit.default_timer()
     print(f"runtime was: {(end-start) * 10**3} milliseconds")
     tracemalloc.start()
-    q = knapsack_recur(C, n, w, p)
+    q = knapsack_recur_unbounded(C, n-1, w, p)
     print(f"{tracemalloc.get_traced_memory()[1]} bytes")
     tracemalloc.stop()
